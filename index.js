c02df1c888d1136952d9b9dcb443bdfcf04148ef
Dave Rodriguez <root01x@gmail.com>
2021-08-27 17:35:26 -0500

    consumo de datos API de WOW exitos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://jsonplaceholder.typicode.com';
+/*const API_URL = 'https://jsonplaceholder.typicode.com';
 const HTMLResponse = document.querySelector("#app");
 const ul = document.createElement('ul');
 
@@ -9,6 +9,27 @@ fetch(`${API_URL}/users`)
         let elem = document.createElement('li');
         elem.appendChild(
             document.createTextNode(`${user.name}✉${user.email}`)
+        );
+        ul.appendChild(elem);
+    })
+    HTMLResponse.appendChild(ul);
+});*/
+
+
+
+const API_URL ='https://us.api.blizzard.com/data/wow/covenant/index?namespace=static-us&locale=en_US&access_token=USZX0jYfaOTz6ShJr06rQM3FrhDrwffcx2';
+
+//const API_URL = 'https://jsonplaceholder.typicode.com';
+const HTMLResponse = document.querySelector("#app");
+const ul = document.createElement('ul');
+
+fetch(`${API_URL}`)
+.then((response)=>response.json())
+.then((covenant)=>{    
+    covenant['covenants'].forEach(we=>{
+        let elem = document.createElement('li');
+        elem.appendChild(
+            document.createTextNode(`${we.name}✉${we.id}`)
         );
         ul.appendChild(elem);
     })
